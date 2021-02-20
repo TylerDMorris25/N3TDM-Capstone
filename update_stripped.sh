e09ecac44507017c8683885393f80596a814d466
Tyler D Morris <28597345+TylerDMorris25@users.noreply.github.com>
2021-02-20 10:48:34 -0500

    EOL Converted to Unix
    Converted the line endings to Unix format to be compatible with linux. Changed rpi-rw alias to real command so it will run in cron.

--- update_stripped.sh
+++ update_stripped.sh
@@ -3,10 +3,10 @@
 # Script runs automatically via cron.
 
 # Set RPI to Read/Write
-rpi-rw
+sudo mount -o remount,rw / ; sudo mount -o remount,rw /boot
 
 # Get updated stripped.csv file and put it in tmp files
-wget INSERT_URL_HERE -O /tmp/stripped.csv
+wget INSERT_STRiPPED.CSV_URL_HERE -O /tmp/stripped.csv
 
 # Copy udpated stripped.csv file over the old stripped.csv file
 sudo cp /tmp/stripped.csv /usr/local/etc/stripped.csv
